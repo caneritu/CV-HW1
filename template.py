d3164ba9e9e2787410c93b204f882d3caa03d024
caner <isikcan@itu.edu.tr>
2018-10-20 15:00:56 +0300

    menu-bar and toolbar added

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
-    QPushButton, QGroupBox, QAction, QFileDialog
+    QPushButton, QGroupBox, QAction, QFileDialog, qApp
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QLabel
@@ -19,7 +19,7 @@ import cv2
 class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
-        return NotImplementedError
+     #   self.initUI()
 
 
         self.title = 'Histogram Equalization'
@@ -34,13 +34,43 @@ class App(QMainWindow):
         # This function is called when the user clicks File->Target Image.
         return NotImplementedError
 
+    def exitdeneme(self):
+        qApp.quit()
+
     def initUI(self):
-        return NotImplementedError
+
+        self.setGeometry(300, 300, 600, 450)
+        self.setWindowTitle('Histogram Equalization')
+
+        menubar = self.menuBar()
+        menu_file = menubar.addMenu('File')
+
+        open_input = QAction('Open Input', self)
+        open_input.triggered.connect(self.openInputImage())
+
+        open_target = QAction('Open Target', self)
+        open_target.triggered.connect(self.openTargetImage())
+
+        exit_app = QAction('Exit', self)
+        exit_app.triggered.connect(self.exitdeneme)
+
+        histogram_equalize = QAction('Equalize Histogram', self)
+        histogram_equalize.triggered.connect(self.histogramButtonClicked())
+
+        menu_file.addAction(open_input)
+        menu_file.addAction(open_target)
+        menu_file.addAction(exit_app)
+
+
+        self.toolbar = self.addToolBar('toolbar')
+        self.toolbar.addAction(histogram_equalize)
+
         # Write GUI initialization code
 
         self.show()
 
     def histogramButtonClicked(self):
+        return NotImplementedError
         if not self.inputLoaded and not self.targetLoaded:
             # Error: "First load input and target images" in MessageBox
             return NotImplementedError
@@ -70,5 +100,6 @@ class PlotCanvas(FigureCanvas):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
     ex = App()
     sys.exit(app.exec_())
